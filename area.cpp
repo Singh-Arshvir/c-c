c16c42497aa2b0ac8f42e4a0ad2fc11055c0b368
Singh-Arshvir <79410121+Singh-Arshvir@users.noreply.github.com>
2024-08-08 18:57:58 +0530

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int length,breadth;
+    cout<<"Enter length and breadth of rectangle:";
+    cin>>length>>breadth;
+    cout<<"Area of rectangle:"<<length*breadth;
+    return 0;
+}
