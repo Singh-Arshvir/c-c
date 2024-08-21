081b0dcec726fba2f5d823bfde0b439171a04982
Singh-Arshvir <79410121+Singh-Arshvir@users.noreply.github.com>
2024-08-21 13:57:18 +0530

    Add files via upload

---
+++ ascivalue.cpp
@@ -0,0 +1,14 @@
+// C++ program to print
+// ASCII Value of Character
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+    char c = 'A';
+    cout << "The ASCII value of " << 
+             c << " is " << int(c);
+            //  int(variable) is used to find ascii value of character 
+    return 0;
+}
